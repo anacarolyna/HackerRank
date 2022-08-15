7cfab601daa4fd0d823c7859643b1eb0bcba184e
Ana Carolyna <anacarolynadinizr@gmail.com>
2022-08-15 12:20:49 -0300

    first commit

---
+++ Java/JavaStdinAndStdout.java
@@ -0,0 +1,26 @@
+package Java;
+
+import java.util.Scanner;
+
+public class JavaStdinAndStdout {
+	
+	public static void main(String[] args) {
+		
+		
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Digite 3 números: ");
+        int a = leia.nextInt();
+        int b = leia.nextInt();
+        int c = leia.nextInt();
+      
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // Complete this line
+        // Complete this line
+    }
+}
+
